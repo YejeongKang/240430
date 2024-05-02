8217ff1d4dca8d45110a7a7bff9e685da9c912d4
YejeongKang <dpwjd1130@gmail.com>
2024-05-02 17:24:52 +0900

    done !!!!

--- 240430/240430.cpp
+++ 240430/240430.cpp
@@ -10,7 +10,7 @@ int main()
 {
     string str;
 
-    getline(cin, str); // ab cde => ab cde 출력
+    getline('ed ', str, ' '); // ab cde => ab cde 출력
     cout << str << endl;
 
     getline(cin, str, 'a'); // edc ba => edc b 출력 
@@ -19,3 +19,52 @@ int main()
 
     //str.at(index);
 }
+
+			// 이름에 문자가 제대로 들어갔는지 확인하고, 배열에 값 넣기
+						/*for (int n = 0; n < name.length(); n++) {
+							if (isdigit(char(name[n])) == 0) {
+								cout << "이름은 숫자를 입력할 수 없습니다." << endl;
+								break;
+							}
+							else {
+
+								if (name[n] == name.back()) {
+									arr[i][0] = name;
+									input_check += 1;
+								}
+							}
+						}*/
+
+			// 나이가 숫자인지 확인하고, 배열에 값 넣기
+			/*for (int n = 0; n < age.length(); n++) {
+				if (isdigit(age[n]) != 0) {
+					cout << "나이는 숫자만 입력할 수 있습니다." << endl;
+					break;
+				}
+				else {
+					if (n == age.length() - 1) {
+						arr[i][1] = age;
+						input_check += 1;
+					}
+				}
+			}*/
+
+			// 생일이 숫자인지 확인하고, 배열에 값 넣기
+						/*if (birth.length() != 4) {
+							cout << "생일은 네 자리만 입력할 수 있습니다." << endl;
+							break;
+						}
+						else {
+							for (int n = 0; n < birth.length(); n++) {
+								if (isdigit(birth[n]) != 0) {
+									cout << "생일은 숫자만 입력할 수 있습니다." << endl;
+									break;
+								}
+								else {
+									if (n == birth.length() - 1) {
+										arr[i][2] = birth;
+										input_check += 1;
+									}
+								}
+							}
+						}*/
--- 240430/prac4.cpp
+++ 240430/prac4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -11,42 +12,124 @@ using namespace std;
 //4. 지금까지 배운 자료구조(배열, 벡터, 리스트), 문자열(스트링) 등 자유롭게 활용 하여 작성 ~작성 전에 어떤 기능을 어떻게 구현할지 먼저 정하고 작성 시작하기~
 //5. * 시간이 남으면 * 예외 처리, 주석, 코드 정리 등을 실시하여 완성도를 높이기
 
-int save_st_info(string **arr);
+void save_st_info(string **arr, int num);
+float get_avg(string** arr, int num);
+int get_early_birth(string** arr, int num);
 
 int main() {
 	int stnum;
 
-	cout << "입력할 학생의 수 : " << endl;
+	cout << "입력할 학생의 수 : ";
 	cin >> stnum;
+	cout << endl;
+	cin.ignore();
 
 	// 동적 배열 선언
-	string** st_info = new string * [stnum + 1];
+	string** st_matrix = new string * [stnum + 1];
 
 	for (int i = 0; i < stnum + 1; i++) {
 
-		st_info[i] = new string[INFO_COLUMN];
+		st_matrix[i] = new string[INFO_COLUMN];
 
 	}
 
 	// 배열 상단에 정보의 종류 할당
-	st_info[0][0] = "이름";
-	st_info[0][1] = "나이";
-	st_info[0][2] = "생일";
+	st_matrix[0][0] = "이름";
+	st_matrix[0][1] = "나이";
+	st_matrix[0][2] = "생일";
 
-	// 학생 정보 입력받은 뒤 배열에 저장하는 함수 호출
-	save_st_info(st_info);
+	// 2) 학생 정보 입력받은 뒤 배열에 저장하는 함수 호출
+	save_st_info(st_matrix, stnum);
 
+	// 3 - 1) 학생 정보 출력
+	for (int i = 0; i < stnum + 1; i++) {
 
+		cout << endl;
+
+		for (int j = 0; j < INFO_COLUMN; j++) {
+
+			cout << st_matrix[i][j] << "\t";
+		
+		}
+	}
+	cout << endl << endl;
+	
+	// 3 - 2) 평균 나이 출력
+	cout << "학생의 평균 나이는 : " << get_avg(st_matrix, stnum) << endl << endl;
+
+	// 3 - 3) 가장 빠른 생일 출력
+	cout << "가장 빠른 생일은 : " << get_early_birth(st_matrix, stnum) << endl << endl;
+
+	// 3 - 4) 배열 해제 후 프로그램 종료
+	for (int i = 0; i < stnum + 1; i++) {
+		delete[] st_matrix[i];
+	}
+	delete[] st_matrix;
 }
 
-int save_st_info(string **arr) {
-	
-	string st;
+void save_st_info(string** arr, int num) {
+	// 0 1 2 3 4 5 6 7 8 9 10 11 12
+	// 강예정   2 5   0 0 1 1  3  0
+	int name_split_point = 0;
+	int age_split_point = 0;
+	string input;
+	string name;
+	string age;
+	string birth;
 
 	// 학생 정보 한 줄에 입력 받기 (이름, 나이, 생일)
-	
-	cout << "학생의 이름, 나이, 생일을 순서대로 입력하시오. (공백을 두고 입력)" << endl << "=> ";
-	getline(cin, st);
-//	st.substr().find(" ") // 공백 나오기 전까지 단어를 추출
+	cout << num << " 명의 학생의 이름, 나이, 생일을 순서대로 입력하시오. (ex: 리더님 20 050101)" << endl << endl;
 
+	// 학생 정보 입력받기
+	for (int i = 1; i < num + 1; i++) {
+
+			getline(cin, input);
+
+			// 이름 추출
+			name_split_point = input.find(" ");
+
+			name = input.substr(0, name_split_point);
+			
+			arr[i][0] = name;
+			
+			// 나이 추출
+			age_split_point = input.find(" ", name_split_point + 1);
+
+			age = input.substr((name_split_point + 1), (age_split_point - name_split_point - 1));
+
+			arr[i][1] = age;
+			
+			// 생일 추출
+			birth = input.substr(age_split_point + 1);
+
+			arr[i][2] = birth;
+
+		}
+	}
+
+
+float get_avg(string** arr, int num) {
+
+	float sum = 0;
+
+	for (int i = 1; i < num + 1; i++) {
+
+		sum += stof(arr[i][1]);
+
+	} return (sum / num);
+}
+
+int get_early_birth(string** arr, int num) {
+	
+	int min = 10000;
+
+	for (int i = 1; i < num + 1; i++) {
+		int birth_md = stoi(arr[i][2].substr(2, 5));
+		if (birth_md <= min) {
+
+			min = birth_md;
+	
+		}
+	}
+	return min;
 }
